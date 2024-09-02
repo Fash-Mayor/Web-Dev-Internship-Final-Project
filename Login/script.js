72af61a8c963bcbea86f3c840ac54941c793f571
Fash Mayor <mayowasamson03@gmail.com>
2024-09-02 22:03:41 +0100

    Updated text in indexes

---
+++ Login/script.js
@@ -0,0 +1,7 @@
+document.getElementById("signup").addEventListener("click", function() {
+    document.getElementById("form").style.display = "none";
+
+    document.getElementById("form").style.display = "none";
+
+  });
+  

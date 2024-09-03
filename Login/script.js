e87dfe26d5a88fe0f3e77415fb83038c95e66565
Fash Mayor <mayowasamson03@gmail.com>
2024-09-03 22:18:00 +0100

    Modified Login/Sign up page

--- Login/script.js
+++ Login/script.js
@@ -1,7 +1,14 @@
 document.getElementById("signup").addEventListener("click", function() {
     document.getElementById("form").style.display = "none";
 
-    document.getElementById("form").style.display = "none";
+    document.getElementById("form2").style.display = "block";
+
+  });
+  
+  document.getElementById("login").addEventListener("click", function() {
+    document.getElementById("form2").style.display = "none";
+
+    document.getElementById("form").style.display = "block";
 
   });
   

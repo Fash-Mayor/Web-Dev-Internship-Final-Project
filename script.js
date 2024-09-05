412ec60b7ad5137c75957ab83b1ee4c5539601a0
Fash Mayor <mayowasamson03@gmail.com>
2024-09-05 21:46:20 +0100

    moved the styles to a new style.css file

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-document.getElementById("signup").addEventListener("click", function() {
+document.getElementById("signup").addEventListener("click", function () {
     document.getElementById("form").style.display = "none";
     document.getElementById("form2").style.display = "block";
 });
-  
-document.getElementById("login").addEventListener("click", function() {
+
+document.getElementById("login").addEventListener("click", function () {
     document.getElementById("form2").style.display = "none";
     document.getElementById("form").style.display = "block";
 });
-  
-document.getElementById('first_login').addEventListener("click", function() {
-    let usern1 = document.getElementById('username1');
-    let pass1 = document.getElementById('password1');
 
-    if(usern1.value.trim() === '' && pass1.value.trim() === ''){
+let usern1 = document.getElementById('username1');
+let pass1 = document.getElementById('password1');
+
+document.getElementById('first_login').addEventListener("click", function () {
+    // event.preventDefault();
+
+    if (pass1.value.trim() == '') {
         Event.preventDefault;
     }
-    else{
-        document.getElementsByClassName('link').href = 'Main/main.html';
+    else {
+        document.location.href = 'Main/main.html';
     }
-})
+
+});
